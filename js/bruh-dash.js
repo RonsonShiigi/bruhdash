c3dbbb40e71f7ff8dee819855678d23f0e2b8f75
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-12 23:30:09 -1000

    pau with  drop()

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,43 +10,64 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+      return arr.shift();
   },
 
   // returns the last element of an array
-  last: function () {
+  last: function (arr) {
+    return arr.pop()
 
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr,x) { 
+   return arr.indexOf(x)
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (arr,x) {
+    return arr.lastIndexOf(x)
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    arr.pop();
+    return arr
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
+  compact: function(arr) {
+    var final = []
+    for(var i=0; i<arr.length; i++){
+      if(arr[i]){
+        final.push(arr[i])
+      }
+      
+    }
+    // console.log(final)
+    return final;
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr) {
+    // console.log(arr)
+    var final = arr.slice(2,4)
+    return final
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(arr,n){
+    if(typeof n !== 'number'){
+      arr.shift();
+      return arr
+    }else if(n===0){
+      return arr
+    }else if(typeof n === 'number'){
+      for(var i=0; i<n; i++){
+        arr.shift();
+      }return arr;
+    }
   },
 
   // returns a slice of array with n elements dropped from the end
